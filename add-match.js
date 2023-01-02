e945312772ff71aa9f628f3f4f36dd07085ed342
Ahmed Fathy <ahmedfathy1234553@gmail.com>
2023-01-02 12:35:03 +0200

    read input

--- add-match.js
+++ add-match.js
@@ -1,8 +1,8 @@
 function set_match() {
-  var match_name = document.forms["stad"]["match_name"].value;
-  var match_date = document.forms["stad"]["match_date"].value;
-  var match_time = document.forms["stad"]["match_time"].value;
-  var match_stadium = document.forms["stad"]["match_stadium"].value;
-  var match_team1 = document.forms["stad"]["match_team1"].value;
-  var match_team2 = document.forms["stad"]["match_team2"].value;
+  var match_name = document.forms["match"]["match_name"].value;
+  var match_date = document.forms["match"]["match_date"].value;
+  var match_time = document.forms["match"]["match_time"].value;
+  var match_stadium = document.forms["match"]["match_stadium"].value;
+  var match_team1 = document.forms["match"]["match_team1"].value;
+  var match_team2 = document.forms["match"]["match_team2"].value;
 }
--- add-satd.js
+++ add-satd.js
@@ -1,2 +1,8 @@
-
-var match_name = document.forms["myForm"]["match_name"].value;
+function set_stad() {
+  var stad_name = document.forms["stad"]["stad_name"].value;
+  var stad_date = document.forms["stad"]["stad_Capacity"].value;
+  var stad_time = document.forms["stad"]["stad_MaxRows"].value;
+  var stad_stadium = document.forms["stad"]["stad_MaxColumns"].value;
+  var stad_team1 = document.forms["stad"]["stad_team1"].value;
+  var stad_team2 = document.forms["stad"]["stad_team2"].value;
+}
